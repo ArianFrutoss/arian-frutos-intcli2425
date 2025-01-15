48190ac82dd2eb6c82c43e4a02aa6a4c39397809
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-01-15 10:10:29 +0100

    ADD - the getPotionByRarity function and test it

--- src/helpers/potionHelpers.ts
+++ src/helpers/potionHelpers.ts
@@ -3,3 +3,7 @@ import { Potion } from "../types/Potion";
 export const filterByLevelRequirement = (potions: Potion[], level: number) => {
     return potions.filter((potion: Potion) => potion.usage.restrictions.levelRequirement <= level);
 }
+
+export const getPotionsByRarity = (potions: Potion[], rarity: string) => {
+    return potions.filter((potion: Potion) => potion.rarity === rarity);
+}
--- src/test/potionHelpers.test.ts
+++ src/test/potionHelpers.test.ts
@@ -1,5 +1,5 @@
 import { potions } from "../data/data";
-import { filterByLevelRequirement } from "../helpers/potionHelpers";
+import { filterByLevelRequirement, getPotionsByRarity } from "../helpers/potionHelpers";
 
 describe('When the level is higher than the potion requirement level', () => {
     it('Should return potions with level_requirement lower than level', () => {
@@ -9,5 +9,16 @@ describe('When the level is higher than the potion requirement level', () => {
         const filteredPotions = filterByLevelRequirement(potionsArray, level);
 
         expect(filteredPotions).toStrictEqual([potionsArray[0], potionsArray[2], potionsArray[5]]);
+    });
+});
+
+describe('When the rarity it is the same as the potion rarity', () => {
+    it('Should return potions with the same rarity', () => {
+        const rarity = "mythic";
+        const potionsArray = potions;
+
+        const filteredPotions = getPotionsByRarity(potionsArray, rarity);
+
+        expect(filteredPotions).toStrictEqual([potionsArray[7], potionsArray[9]]);
     });
 });
