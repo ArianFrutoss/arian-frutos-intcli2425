7dacac1ffb1f0335a856b62ceb83cede677dc1ba
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-01-15 12:06:15 +0100

    ADD - the render of the potions

---
+++ src/components/PotionList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Potion } from "../types/Potion";
+
+interface Props {
+  potionsArray: Potion[];
+}
+
+const PotionList: React.FC<Props> = ({ potionsArray }) => {
+  return (
+    <>
+      <div className="w-[100%] bg-gray-950 bg-opacity-50 p-[2%] flex flex-wrap">
+          {potionsArray.map((potion, i) => (
+            <div key={i} className="bg-black w-[20%] h-[10%] p-[2%]">
+              <p>{potion.name}</p>
+              <img src={`../../../public/imgs/${potion.image}`} alt={potion.id} />
+              <p>{potion.rarity}</p>
+              <p>{potion.meta.availability.drop_rate.boss}</p>
+              <p>{potion.meta.availability.drop_rate.chance}</p>
+            </div>
+          ))}
+      </div>
+    </>
+  )
+}
+
+export default PotionList;
